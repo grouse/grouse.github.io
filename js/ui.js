0e24e0ab352a98682c02441db48762471cd81cdd
Jesper Stefansson <jesper.stefansson@gmail.com>
2019-06-01 20:00:29 +0200

    moving this back from gitlab

---
+++ js/ui.js
@@ -0,0 +1,32 @@
+(function (window, document) {
+	var layout   = document.getElementById("layout"),
+	    menu_btn = document.getElementById("menu_btn"),
+	    menu     = document.getElementById("menu");
+
+	function toggleClass(element, className) {
+		var classes = element.className.split(/\s+/),
+		length = classes.length,
+		i = 0;
+
+		for(; i < length; i++) {
+			if (classes[i] === className) {
+				classes.splice(i, 1);
+				break;
+			}
+		}
+		// The className is not found
+		if (length === classes.length) {
+			classes.push(className);
+		}
+		element.className = classes.join(' ');
+	}
+
+	menu_btn.onclick = function(e) {
+		e.preventDefault();
+
+		var active = 'active';
+		toggleClass(menu, active);
+		toggleClass(menu_btn, active);
+		toggleClass(layout, active);
+	};
+}(this, this.document));
